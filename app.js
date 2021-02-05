53ef19c7244786c5717fa214459dffba51503786
Jonathan Baez <jbaez9910@gmail.com>
2021-02-04 23:06:19 -0500

    Comunicación entre Componentes: propiedades

--- app.js
+++ app.js
@@ -1,48 +1,22 @@
-new Vue({
-  el: '#app',
-
+Vue.component('CoinDetail', {
+  props:['coin'],
   data(){
     return {
-      name:'Bitcoin',
-      symbol:'BTC',
-      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent:6,
-      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
-      price: 8400,
-      color:'f4f4f4',
+      showPrices:false,
       value:0,
 
-      pricesWithDays: [
-        { day: 'Lunes', value: 8400 },
-        { day: 'Martes', value: 7900 },
-        { day: 'Miercoles', value: 8200 },
-        { day: 'Jueves', value: 9000 },
-        { day: 'Viernes', value: 9400 },
-        { day: 'Sabado', value: 10000 },
-        { day: 'Domingo', value: 10200 },
-      ],
-      showPrices:false,
-
     }
-    
-    
   },
-  computed: {
-    //funciones que siempre devulven un valor
+  computed:{
+     //funciones que siempre devulven un valor
     title(){
-      return `${this.name} - ${this.symbol}`;
+      return `${this.coin.name} - ${this.coin.symbol}`;
     },
     convervetedValue(){
       if(!this.value){
         return 0;
       }
-      return this.value/this.price
-    }
-  },
-  watch:{
-    //funciones el nombre de la funcion debe corresponder con una propiedad de la data()
-    showPrices(newVal, oldVal){
-      console.log(newVal, oldVal)
+      return this.value/this.coin.price
     }
   },
   methods:{
@@ -51,6 +25,84 @@ new Vue({
 
       this.color = this.color.split('').reverse().join('')
     }
-}
+  },
+  template:`
+    <div>
+        <img v-on:mouseover="toggleShowPrices" v-on:mouseout="toggleShowPrices" v-bind:src="coin.img" v-bind:alt="coin.name">
+
+        <h1 v-bind:class="coin.changePercent > 5 ? 'green' : 'red'">{{title}}
+          <span v-if="coin.changePercent > 5">👍</span>
+          <span v-else>👎</span>
+
+          <span v-on:click="toggleShowPrices">{{showPrices ? '😑' : '😋'}}</span>
+
+        </h1>
+
+        <input type="number" v-model="value">
+        <span>{{convervetedValue}}</span>
+
+        <ul v-show="showPrices">
+            <li v-for="(p,i) in prices" v-bind:key="p">{{i}}--{{p}}</li>
+        </ul>
+        <ul v-show="showPrices">
+            <li class="uppercase" 
+                v-bind:class="{orange: p.value == coin.price, red : p.value < coin.price, green : p.value > coin.price}" 
+                v-for="(p,i) in coin.pricesWithDays" 
+                v-bind:key="p.day">
+                {{i}}--{{p.value}}-{{p.day}}
+            </li>
+        </ul>
+    </div>
+    
+  `
+})
+
+
+new Vue({
+  el: '#app',
+
+  data(){
+    return {
+      btc:{
+        name:'Bitcoin',
+        symbol:'BTC',
+        img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+        changePercent:6,
+        price: 8400,
+        prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+        pricesWithDays: [
+            { day: 'Lunes', value: 8400 },
+            { day: 'Martes', value: 7900 },
+            { day: 'Miercoles', value: 8200 },
+            { day: 'Jueves', value: 9000 },
+            { day: 'Viernes', value: 9400 },
+            { day: 'Sabado', value: 10000 },
+            { day: 'Domingo', value: 10200 },
+        ]
+      },
+      
+      color:'f4f4f4',
+    
+    }
+    
+    
+  },
+  computed: {
+   
+    
+  },
+  watch:{
+    // //funciones el nombre de la funcion debe corresponder con una propiedad de la data()
+    // showPrices(newVal, oldVal){
+    //   console.log(newVal, oldVal)
+    // }
+  }
+  // methods:{
+  //   toggleShowPrices(){
+  //     this.showPrices = !this.showPrices; 
+
+  //     this.color = this.color.split('').reverse().join('')
+  //   }
+  //  }
  
 })
